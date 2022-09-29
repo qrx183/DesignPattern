bc155ca090e2346da826f9bf0560be6c680ef685
邱瑞翔 <1330342818@qq.com>
2022-09-29 20:43:53 +0800

    克隆模式

---
+++ com/qrx/designpattern/prototype/Sheep.java
@@ -0,0 +1,70 @@
+package com.qrx.designpattern.prototype;
+
+import java.util.Calendar;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Sheep implements Cloneable{
+
+    private String name;
+    private String color;
+    private int age;
+
+    public Sheep(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Sheep sheep = null;
+        sheep =(Sheep)super.clone();
+        return sheep;
+  }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+//        Sheep sheep = new Sheep("first","white",8);
+//        Sheep sheep1 = (Sheep)sheep.clone();
+//        Sheep sheep2 = (Sheep)sheep.clone();
+//        System.out.println(sheep1 == sheep);
+//        System.out.println(sheep1.getColor() == sheep.getColor());
+
+    }
+}
+
